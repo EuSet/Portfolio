05e808c66ac5d9b4273d5744cca8f56e7520d3b9
EuSet <setskoeugene@inbox.ru>
2021-05-31 20:11:26 +0300

    added font

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,19 +1,17 @@
 import React from "react"
 import m from './Main.module.css'
 import c from '../styles/Container.module.css'
+import mainPhoto from "../../imges/Main.jpg"
 
 export const Main = () => {
     return <div className={m.main}>
         <div className={`${c.container} ${m.mainContainer}`}>
             <div className={m.description}>
                 <span>Hi there</span>
-                <h1>I am Jeffery Aaron</h1>
+                <h1>I am Eugene Setsko</h1>
                 <p>Frontend developer</p>
             </div>
-            <div className={m.photo}></div>
-
-
+            <div><img className={m.photo} src={mainPhoto} alt={'#'}/></div>
         </div>
-
     </div>
 }
